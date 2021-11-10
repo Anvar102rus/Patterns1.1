cc70da2913ec4330bb35269b679fe0e02bac8929
AnvarPro <anvarvahitov30@rambler.com>
2021-11-10 23:40:15 +0500

    Test

---
+++ src/test/java/ru/netology/AuthTest.java
@@ -0,0 +1,68 @@
+package ru.netology;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class AuthTest {
+    private final PersonGanerator.AuthUser activeUser = PersonGanerator.getValidActiveUser();
+    private final PersonGanerator.AuthUser blockedUser = PersonGanerator.getValidBlockedUser();
+    private final PersonGanerator.AuthUser invalidPassword = PersonGanerator.getInvalidPassword();
+    private final PersonGanerator.AuthUser invalidLogin = PersonGanerator.getInvalidLogin();
+
+    @BeforeEach
+    void setupClass() {
+        open("http://localhost:9999/");
+    }
+
+    @Test
+    void shouldSendValidActiveUser() {
+        $("[data-test-id=login] .input__control").sendKeys(activeUser.getLogin());
+        $("[data-test-id=password] .input__control").sendKeys(activeUser.getPassword());
+        $("[data-test-id=action-login]").click();
+        $(byText("Личный кабинет")).shouldBe(visible, Duration.ofSeconds(15));
+    }
+
+    @Test
+    void shouldSendValidBlockedUser(){
+        $("[data-test-id=login] .input__control").sendKeys(blockedUser.getLogin());
+        $("[data-test-id=password] .input__control").sendKeys(blockedUser.getPassword());
+        $("[data-test-id=action-login]").click();
+        $(".notification_status_error").shouldBe(visible, Duration.ofSeconds(15));
+        $("[data-test-id=error-notification]  .notification__title")
+                .shouldHave(exactText("Ошибка"));
+        $("[data-test-id=error-notification]  .notification__content")
+                .shouldHave(exactText("Ошибка! Пользователь заблокирован"));
+    }
+
+    @Test
+    void shouldSendInvalidPassword(){
+        $("[data-test-id=login] .input__control").sendKeys(invalidPassword.getLogin());
+        $("[data-test-id=password] .input__control").sendKeys(invalidPassword.getPassword());
+        $("[data-test-id=action-login]").click();
+        $("[data-test-id=error-notification]").shouldBe(visible, Duration.ofSeconds(15));
+        $("[data-test-id=error-notification]  .notification__title")
+                .shouldHave(exactText("Ошибка"));
+        $("[data-test-id=error-notification]  .notification__content")
+                .shouldHave(exactText("Ошибка! Неверно указан логин или пароль"));
+    }
+
+    @Test
+    void shouldSendInvalidLogin(){
+        $("[data-test-id=login] .input__control").sendKeys(invalidLogin.getLogin());
+        $("[data-test-id=password] .input__control").sendKeys(invalidLogin.getPassword());
+        $("[data-test-id=action-login]").click();
+        $("[data-test-id=error-notification]").shouldBe(visible, Duration.ofSeconds(15));
+        $("[data-test-id=error-notification]  .notification__title")
+                .shouldHave(exactText("Ошибка"));
+        $("[data-test-id=error-notification]  .notification__content")
+                .shouldHave(exactText("Ошибка! Неверно указан логин или пароль"));
+    }
+}
